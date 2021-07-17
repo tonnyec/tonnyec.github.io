37a57fb52554f3ea7060a81228adcdccf5ae1dcb
tonny <lb@mantis.ec>
2021-07-16 21:38:48 -0500

    Factor de seguridad con carga estatica

--- resortes.js
+++ resortes.js
@@ -1,14 +1,15 @@
-$("#btnCalcular").click(function () {
-  alert("HOla");
+$("#btn_calcular").click(function () {
   var fuerza_inicial = $("#fuerza-inicial").val();
   var fuerza_final = $("#fuerza-final").val();
-  var diametro = $("#diametro").val();
+  var d = $("#diametro").val();
   var y = $("#deflexion").val();
+  var porcentaje_choque = $("#porcentaje-choque").val();
   var c = $("#c").val();
-  var b = $("#c").val();
-  var a = $("#c").val();
+  var b = $("#b").val();
+  var a = $("#a").val();
+  
 
-  var diametro_medio_D = c * diametro;
+  var diametro_medio_D = c * d;
   console.log(diametro_medio_D);
 
   var ks = 1 + 0.5 / c;
@@ -30,13 +31,13 @@ $("#btnCalcular").click(function () {
   var nt = na + 2;
   console.log(nt);
 
-  var ls = diametro * nt;
+  var ls = d * nt;
   console.log(ls);
 
   var y_trabajo = fuerza_inicial / k;
   console.log(y_trabajo);
 
-  var y_choque = 0.1 * y_trabajo;
+  var y_choque = porcentaje_choque * y_trabajo;
   console.log(y_choque);
 
   var fuerza_precarga = 5;
@@ -46,5 +47,16 @@ $("#btnCalcular").click(function () {
   var lf = ls + y_choque + y_trabajo + y_inicial;
   console.log(lf);
 
+  var y_cierre = lf - ls;
+  console.log(y_cierre);
 
+  var f_cierre = k * y_cierre;
+  console.log(f_cierre);
+
+  var t_max =
+    ks * ((8 * diametro_medio_D * f_cierre) / (Math.PI * Math.pow(d, 3)));
+  console.log(t_max);
+
+  var ns = sys / t_max;
+  console.log(ns);
 });
