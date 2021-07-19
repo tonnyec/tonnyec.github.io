d2e2c092831178507d9a1e0ced4461e8a0bcc1c0
tonny <lb@mantis.ec>
2021-07-19 12:10:05 -0500

    pruebas

--- resorte_traccion.js
+++ resorte_traccion.js
@@ -6,19 +6,20 @@ $("#btn_nuevo").click(function () {
 
 $("#btn_calcular").click(function () {
   DECIMALS = $("#txt-decimales").val();
-  var fuerza_min = $("#fuerza-min").val();
-  var d = $("#diametro").val();
-  var y = $("#deflexion").val();
-  var porcentaje_choque = $("#porcentaje-choque").val();
-  var c = parseFloat($("#c").val());
-  var b = parseFloat($("#b").val());
-  var a = parseFloat($("#a").val());
+  let F = parseFloat($("#fuerza-min").val());
+  let d = parseFloat($("#diametro").val());
+  let r2 = parseFloat($("#r2").val());
+  let y = parseFloat($("#deflexion").val());
+  let porcentaje_choque = $("#porcentaje-choque").val();
+  let c = parseFloat($("#c").val());
+  let b = parseFloat($("#b").val());
+  let a = parseFloat($("#a").val());
 
-  var diametro_medio_D = c * d;
-  $("#txt-diametro-medio").val(diametro_medio_D.toFixed(DECIMALS));
-  console.log(diametro_medio_D);
+  let D = c * d;
+  $("#txt-diametro-medio").val(D.toFixed(DECIMALS));
+  console.log(D);
 
-  var ks = 1 + 0.5 / c;
+  let ks = 1 + 0.5 / c;
   $("#txt-ks").val(ks.toFixed(DECIMALS));
   console.log(ks);
 
@@ -27,29 +28,27 @@ $("#btn_calcular").click(function () {
   $("#txt-ti-1").val(ti.toFixed(DECIMALS));
   console.log(ti);
 
-  let fi = (ti * Math.PI * Math.pow(d, 3)) / (8 * ks * diametro_medio_D);
+  let fi = (ti * Math.PI * Math.pow(d, 3)) / (8 * ks * D);
   $("#txt-fi-1").val(fi.toFixed(DECIMALS));
   console.log(ti);
 
-  var sut = a * Math.pow(d, b);
+  let sut = a * Math.pow(d, b);
   $("#txt-sut").val(sut.toFixed(DECIMALS));
   console.log(sut);
 
-  var sys = 0.45 * sut;
+  let sys = 0.45 * sut;
   $("#txt-sys").val(sys.toFixed(DECIMALS));
   console.log(sys);
 
-  var k = (fuerza_min - fi) / y;
+  let k = (F - fi) / y;
   $("#txt-k").val(k.toFixed(DECIMALS));
   console.log(k);
 
-  var na =
-    (Math.pow(d, 4) * 11500000) / (8 * k * Math.pow(diametro_medio_D, 3));
+  let na = (Math.pow(d, 4) * 11500000) / (8 * k * Math.pow(D, 3));
   $("#txt-na").val(na.toFixed(DECIMALS));
   console.log(na);
 
-  let tmax =
-    (ks * (8 * diametro_medio_D * fuerza_min)) / (Math.PI * Math.pow(d, 3));
+  let tmax = (ks * (8 * D * F)) / (Math.PI * Math.pow(d, 3));
   $("#txt-tmax-1").val(tmax.toFixed(DECIMALS));
   console.log(na);
 
@@ -57,33 +56,55 @@ $("#btn_calcular").click(function () {
   $("#txt-ns-1").val(ns.toFixed(DECIMALS));
   console.log(ns);
 
+  let kb = (4 * Math.pow(c, 2) - c - 1) / (4 * c * (c - 1));
+  $("#txt-kb").val(kb.toFixed(DECIMALS));
+  console.log(kb);
 
+  let ta =
+    kb * ((16 * F * D) / (Math.PI * Math.pow(d, 3))) +
+    (4 * F) / (Math.PI * Math.pow(d, 2));
+  $("#txt-ta").val(ta.toFixed(DECIMALS));
+  console.log(ta);
 
+  let nA = (0.75 * sut) / ta;
+  $("#txt-na-1").val(nA.toFixed(DECIMALS));
+  console.log(nA);
 
- 
+  let c2 = (2 * r2) / d;
+  console.log(c2);
+  let kw2 = (4 * c2 - 1) / (4 * c2 - 4);
+  console.log(kw2);
+
+  let tb = kw2 * ((8 * F * D) / (Math.PI * Math.pow(d, 3)));
+  $("#txt-tb").val(tb.toFixed(DECIMALS));
+  console.log(tb);
+
+  let nB = (0.75 * sut) / ta;
+  $("#txt-nb-1").val(nB.toFixed(DECIMALS));
+  // console.log(nB);
 });
 
 $("#btn_calcular_fatiga").click(function () {
   DECIMALS = $("#txt-decimales").val();
-  var fmin = parseFloat($("#fuerza-min").val()); // Fi
-  var fmax = parseFloat($("#fuerza-max").val()); // Fm
+  let fmin = parseFloat($("#fuerza-min").val()); // Fi
+  let fmax = parseFloat($("#fuerza-max").val()); // Fm
   let fi = fmin;
 
-  var d = $("#diametro").val();
-  var y = $("#deflexion").val();
-  var porcentaje_choque = $("#porcentaje-choque").val();
-  var c = $("#c").val();
-  var b = $("#b").val();
-  var a = $("#a").val();
+  let d = $("#diametro").val();
+  let y = $("#deflexion").val();
+  let porcentaje_choque = $("#porcentaje-choque").val();
+  let c = $("#c").val();
+  let b = $("#b").val();
+  let a = $("#a").val();
 
-  var kw = (4 * c - 1) / (4 * c - 4) + 0.615 / c;
+  let kw = (4 * c - 1) / (4 * c - 4) + 0.615 / c;
   $("#txt-kw").val(kw.toFixed(DECIMALS));
   console.log(kw);
 
   let fm = (fmax + fmin) / 2;
   $("#txt-fm").val(fm.toFixed(DECIMALS));
   console.log(fm);
-  var fa = (fmax - fmin) / 2;
+  let fa = (fmax - fmin) / 2;
   $("#txt-fa").val(fa.toFixed(DECIMALS));
   console.log(fa);
 
@@ -93,32 +114,32 @@ $("#btn_calcular_fatiga").click(function () {
   $("#txt-ti").val(ti.toFixed(DECIMALS));
   console.log(ti);
 
-  var tm = ks * ((8 * fm * D) / (Math.PI * Math.pow(d, 3)));
+  let tm = ks * ((8 * fm * D) / (Math.PI * Math.pow(d, 3)));
   $("#txt-tm").val(tm.toFixed(DECIMALS));
   console.log(tm);
 
-  var ta = ks * ((8 * fa * D) / (Math.PI * Math.pow(d, 3)));
+  let ta = ks * ((8 * fa * D) / (Math.PI * Math.pow(d, 3)));
   $("#txt-ta").val(ta.toFixed(DECIMALS));
   console.log(ta);
 
   // Sew constantes 45000 sin granallar  o 67500 granallado
 
-  var sew = 67500;
+  let sew = 67500;
 
   if (!$("#chk-granallado").is(":checked")) {
     sew = 45000;
   }
 
   let sut = a * Math.pow(d, b); //arriba
-  var sus = 0.67 * sut;
+  let sus = 0.67 * sut;
   $("#txt-sus").val(sus.toFixed(DECIMALS));
   console.log(sus);
 
-  var ses = 0.5 * ((sew * sus) / (sus - 0.5 * sew));
+  let ses = 0.5 * ((sew * sus) / (sus - 0.5 * sew));
   $("#txt-ses").val(ses.toFixed(DECIMALS));
   console.log(ses);
 
-  var nf = (ses * (sus - ti)) / (ses * (tm - ti) + sus * ta);
+  let nf = (ses * (sus - ti)) / (ses * (tm - ti) + sus * ta);
   $("#txt-nf").val(nf.toFixed(DECIMALS));
   console.log(nf);
 });
